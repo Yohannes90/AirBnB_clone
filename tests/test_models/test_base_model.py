200092d116aafffccef1190960a1deb2c8d86757
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-10-15 13:31:22 +0300

    made tests inheritable for use in different modules

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,21 +12,27 @@ class test_baseModel(unittest.TestCase):
     """Defines unittests for models/base_model.py
     """
 
+    def __init__(self, *args, **kwargs):
+        """initalize instance"""
+        super().__init__(*args, *kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
+
     def test_default(self):
         """checks defalt instantiation"""
-        bm = BaseModel()
-        self.assertIsInstance(bm, BaseModel)
+        bm = self.value()
+        self.assertIsInstance(bm, self.value)
 
     def test_kwargs(self):
         """checks two instances with same kwargs are not same instances"""
-        bm = BaseModel()
+        bm = self.value()
         copy = bm.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is bm)
 
     def test_kwargs_int(self):
         """checks kwargs with int arguments"""
-        bm = BaseModel()
+        bm = self.value()
         copy = bm.to_dict()
         copy.update({1 : 2})
         with self.assertRaises(TypeError):
@@ -38,44 +44,38 @@ class test_baseModel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**n)
 
-    # def test_kwargs_one(self):
-    #     """checks if kwargs is given invalid key it raises KeyError exception"""
-    #     n = {'Name': 'test'}
-    #     with self.assertRaises(KeyError):
-    #         new = BaseModel(**n)
-
     def test_id(self):
         """checks id is string"""
-        new = BaseModel()
-        self.assertEqual(type(new.id), str)
+        bm = self.value()
+        self.assertIsInstance(bm.id, str)
 
     def test_created_at(self):
         """checks created_at is type datetime"""
-        new = BaseModel()
-        self.assertEqual(type(new.created_at), datetime.datetime)
+        bm = self.value()
+        self.assertIsInstance(bm.created_at, datetime.datetime)
 
     def test_updated_at(self):
         """checks updated_at is type datetime, not biased when given date as key"""
-        new = BaseModel()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+        bm = self.value()
+        self.assertIsInstance(bm.updated_at, datetime.datetime)
+        n = bm.to_dict()
+        bm = BaseModel(**n)
+        self.assertFalse(bm.created_at == bm.updated_at)
 
     def test_to_str(self):
         """checks __str__ returns desired output"""
-        bm = BaseModel()
-        self.assertEqual(str(bm), '[{}] ({}) {}'.format("BaseModel", bm.id, bm.__dict__))
+        bm = self.value()
+        self.assertEqual(str(bm), '[{}] ({}) {}'.format(self.name, bm.id, bm.__dict__))
 
     def test_todict(self):
         """check to_dict method"""
-        bm = BaseModel()
+        bm = self.value()
         new = bm.to_dict()
         self.assertEqual(bm.to_dict(), new)
 
     def test_save(self):
         """ check save is storing same data as we get from dict"""
-        bm = BaseModel()
+        bm = self.value()
         bm.save()
         key = "BaseModel." + bm.id
         with open('file.json', 'r') as f:
